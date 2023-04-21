157c5a117a390b2a71fad169467792e20099c7cb
troyler <tmreinert22@gmail.com>
2023-04-21 18:12:53 -0400

    more merge resolutions

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,7 +6,10 @@ import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
 import axios from "axios";
 import { UserContextProvider} from './UserContext';
+import ProfilePage from './pages/ProfilePage';
 import ListingsPage from './pages/ListingsPage';
+import ListingsFormPage from './pages/ListingsFormPage';
+import CartPage from './pages/CartPage';
 
 axios.defaults.baseURL = "http://localhost:4000"
 axios.defaults.withCredentials = true;
@@ -19,10 +22,10 @@ function App() {
         <Route index element = {<IndexPage  />}/>
         <Route path = "/login" element = {<LoginPage  />}/>
         <Route path = "/register" element = {<RegisterPage  />}/>
+        <Route path = "/cart" element = {<CartPage  />}/>
         <Route path = "/profile" element = {<ProfilePage  />}/>
         <Route path = "/profile/listings" element = {<ListingsPage  />}/>
         <Route path = "/profile/listings/new" element = {<ListingsFormPage  />}/>
-        <Route path = "/profile/listings/:id" element = {<ListingsFormPage  />}/>
       </Route>
     </Routes>
     </UserContextProvider>

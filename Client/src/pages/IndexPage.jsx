fe54c785db7d9dc422f6e70229bfef790d445a88
troyler <tmreinert22@gmail.com>
2023-04-23 03:30:01 -0400

    working filter

--- Client/src/pages/IndexPage.jsx
+++ Client/src/pages/IndexPage.jsx
@@ -69,7 +69,7 @@ export default function IndexPage() {
           <CartPage cartItems={cartItems} />
         </div>
         <div className = "mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {listings.length > 0 && listings.map(listing => (
+          {filteredListings.length > 0 && filteredListings.map(listing => (
           <Link to = {'/listings/' + listing._id}>
             <div className = "bg-gray-500 mb-2 rounded-2xl flex">
               {listing.addedPhotos?.[0] && (
